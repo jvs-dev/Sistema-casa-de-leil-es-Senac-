0a1b0589a0c7f1d4c578396f8c218438421dc5a9
João Vitor Santana <94786847+jvs-dev@users.noreply.github.com>
2025-01-13 13:07:33 -0300

    Update ProdutosDTO.java

--- src/mainpackage/ProdutosDTO.java
+++ src/mainpackage/ProdutosDTO.java
@@ -1,37 +1,37 @@
 package mainpackage;
 
 public class ProdutosDTO {
-    private Integer id;
+    private int id;
     private String nome;
-    private Integer valor;
+    private int valor;
     private String status;
 
-    public Integer getId() {
+    public int getId() {
         return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getNome() {
         return nome;
     }
 
-    public Integer getValor() {
-        return valor;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
     public void setNome(String nome) {
         this.nome = nome;
     }
 
-    public void setValor(Integer valor) {
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
         this.valor = valor;
+    }
+
+    public String getStatus() {
+        return status;
     }
 
     public void setStatus(String status) {
